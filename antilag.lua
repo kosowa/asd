df8e3b639353091d72c3a638ef194988c114bc5c
kosowa <denverpogi9@gmail.com>
2024-10-27 17:00:47 +0800

    Update antilag.lua

--- antilag.lua
+++ antilag.lua
@@ -114,7 +114,7 @@ end
 
 -- Change Atmosphere color to black if Atmosphere exists
 if game.Lighting:FindFirstChild("Atmosphere") then
-    game.Lighting.Atmosphere.Color = Color3.new(255, 255, 255)
+    game.Lighting.Atmosphere.Color = Color3.new(85, 255, 255)
 end
 
 
