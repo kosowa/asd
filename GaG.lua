9d7cbe55159a7c9d99ac1e41e7000b4c993f37bd
kosowa <denverpogi9@gmail.com>
2026-03-31 23:49:10 +0800

    Update GaG.lua

--- GaG.lua
+++ GaG.lua
@@ -10,7 +10,7 @@ end
 task.wait(math.random())
 
 local loaders = {
-    [35789249] = "https://api.luarmor.net/files/v3/loaders/8b7a53abb55560b7c80c2723c030c5bb.lua", -- GaG
+    [35789249] = "https://raw.githubusercontent.com/kosowa02/v2/refs/heads/main/gago", -- GaG
     [36015593] = "https://api.luarmor.net/files/v3/loaders/3dd0d7e4978bbd5e49aa2662068ab413.lua", -- Hunty Zombies
     [34088633] = "https://api.luarmor.net/files/v3/loaders/a2b73ff8c5bf48a0dce49b0bbf496cad.lua", -- AFS
     [34869880] = "https://api.luarmor.net/files/v3/loaders/d9d8c4ca6f8dec16819206c7d03bec60.lua", --PlantsVsBrainrot
